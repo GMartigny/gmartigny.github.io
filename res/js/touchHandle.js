58447edf7eae9a44ddc1e540d911933e39c8bc60
Guillaume Martigny <guillaume.martigny@gmail.com>
2014-08-06 10:02:39 +0200

    Make it OOP Signed-off-by:Guillaume Martigny <guillaume.martigny@gmail.com>

--- res/js/touchHandle.js
+++ res/js/touchHandle.js
@@ -1,8 +1,3 @@
-
-var _goingTouch = [],
-    _swipeTreshold = 150,
-    _swipeMaxTime = 150;
-
 Array.prototype.out = function(o){
     var p = this.indexOf(o);
     if(p !== -1)
@@ -11,77 +6,118 @@ Array.prototype.out = function(o){
         return false;
 };
 
-document.addEventListener("touchstart", function(ev){
+function Touch(t){
     var now = Date.now();
-    _handleTouch(ev, function(t){
-        var touch = {
-            identifier: t.identifier,
-            target: t.target,
-            startX: t.pageX,
-            startY: t.pageY,
-            startTime: now,
-            pageX: t.pageX,
-            pageY: t.pageY
-        };
-        _goingTouch.push(touch);
+    this.identifier = t.identifier;
+    this.target = t.target;
+    
+    this.startX = t.pageX;
+    this.startY = t.pageY;
+    this.pageX = t.pageX;
+    this.pageY = t.pageY;
+    
+    this.swipe = false;
+    this.swipeDirection = 0;
+    
+    this.startTime = now;
+    
+    this.regTime = now;
+    this.regX = t.pageX;
+    this.regY = t.pageY;
+}
+Touch.UP = 1;
+Touch.RIGHT = 2;
+Touch.DOWN = 4;
+Touch.LEFT = 8;
+
+Touch.onGoingTouch = [];
+Touch.swipeTreshold = 150;
+Touch.swipeMaxTime = 150;
+
+Touch.get = function(id){
+    for(var i=0;i<Touch.onGoingTouch.length;++i)
+        if(Touch.onGoingTouch[i].identifier == id)
+            return Touch.onGoingTouch[i];
+    return null;
+};
+Touch.handleEvent = function(ev, each){
+    var tchs = ev.changedTouches;
+    for(var i=0;i<tchs.length;++i){
+        each(tchs[i]);
+    }
+};
+
+document.addEventListener("touchstart", function(ev){
+    Touch.handleEvent(ev, function(t){
+        var touch = new Touch(t);
+        Touch.onGoingTouch.push(touch);
         
-        if(touch.target.ongrab) touch.target.ongrab(touch);
+        if(touch.target.ongrab)
+            touch.target.ongrab(touch);
         
     });
 }, 0);
 document.addEventListener("touchmove", function(ev){
     var now = Date.now();
-    _handleTouch(ev, function(t){
-        var touch = _getTouch(t.identifier);
+    Touch.handleEvent(ev, function(t){
+        var touch = Touch.get(t.identifier);
         
         if(touch){
             touch.pageX = t.pageX;
             touch.pageY = t.pageY;
 
-            if(touch.target.ondrag) touch.target.ondrag(touch);
+            if(touch.target.ondrag)
+                touch.target.ondrag(touch);
 
-            if(now < touch.startTime + _swipeMaxTime){
-                if(t.pageX < touch.startX - _swipeTreshold) touch.direction = "left";
-                else if(t.pageX > touch.startX + _swipeTreshold) touch.direction = "right";
-                if(t.pageY < touch.startY - _swipeTreshold) touch.direction = "up";
-                else if(t.pageY > touch.startY + _swipeTreshold) touch.direction = "down";
+            if(now - touch.regTime < Touch.swipeMaxTime){
+                touch.direction = 0;
+                if(touch.pageX < touch.regX - Touch.swipeTreshold) touch.direction = Touch.LEFT;
+                else if(touch.pageX > touch.regX + Touch.swipeTreshold) touch.direction = Touch.RIGHT;
+                if(touch.pageY < touch.regY - Touch.swipeTreshold) touch.direction += Touch.UP;
+                else if(touch.pageY > touch.regY + Touch.swipeTreshold) touch.direction += Touch.DOWN;
 
                 if(touch.direction && touch.target.onswipe){
-                    console.log("swipe");
                     touch.target.onswipe(touch);
-                    _goingTouch.out(touch);
                 }
+            }
+            else{
+                touch.regTime = now;
+                touch.regX = touch.pageX;
+                touch.regY = touch.pageY;
             }
         }
     });
 }, 0);
 
 document.addEventListener("touchend", function(ev){
-    _handleTouch(ev, function(t){
-        var touch = _getTouch(t.identifier);
+    Touch.handleEvent(ev, function(t){
+        var touch = Touch.get(t.identifier);
         if(touch){
-            if(touch.target.ondrop) touch.target.ondrop(touch);
-            _goingTouch.out(touch);
+            if(touch.target.ondrop)
+                touch.target.ondrop(touch);
+            Touch.onGoingTouch.out(touch);
         }
     });
 }, 0);
 document.addEventListener("touchcancel", function(ev){
-    _handleTouch(ev, function(t){
-        var touch = _getTouch(t.identifier);
+    Touch.handleEvent(ev, function(t){
+        var touch = Touch.get(t.identifier);
         
         if(touch){
-            if(touch.target.ondrop) touch.target.ondrop(touch);
-            _goingTouch.out(touch);
+            if(touch.target.ondrop)
+                touch.target.ondrop(touch);
+            Touch.onGoingTouch.out(touch);
         }
     });
 }, 0);
 document.addEventListener("touchleave", function(ev){
-    _handleTouch(ev, function(t){
-        var touch = _getTouch(t.identifier);
+    Touch.handleEvent(ev, function(t){
+        var touch = Touch.get(t.identifier);
         
         if(touch){
-            if(touch.target.ondrop) touch.target.ondrop(touch);
-            _goingTouch.out(touch);
+            if(touch.target.ondrop)
+                touch.target.ondrop(touch);
+            Touch.onGoingTouch.out(touch);
         }
     });
 }, 0);
@@ -93,8 +129,8 @@ function _handleTouch(ev, each){
     }
 }
 function _getTouch(id){
-    for(var i=0;i<_goingTouch.length;++i)
-        if(_goingTouch[i].identifier == id)
-            return _goingTouch[i];
+    for(var i=0;i<Touch.onGoingTouch.length;++i)
+        if(Touch.onGoingTouch[i].identifier == id)
+            return Touch.onGoingTouch[i];
     return false;
 }
