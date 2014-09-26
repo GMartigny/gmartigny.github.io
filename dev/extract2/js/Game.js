da007efd43f585786a7660bf033a7037afc3ca36
Guillaume Martigny <guillaume.martigny@gmail.com>
2014-09-26 14:00:02 +0200

    Functionning architecture Signed-off-by:GMartigny <guillaume.martigny@gmail.com>

--- dev/extract2/js/Game.js
+++ dev/extract2/js/Game.js
@@ -1,8 +1,17 @@
 
-function GameManager(canvas){
+function GameManager(media, canvas){
     this.save = new SaveManager();
     this.view = new ViewManager(canvas);
-    this.player = new PlayerController();
+    this.player = new PlayerController(canvas.player.ctx);
+    
+    this.initialX = getById("holder").offsetLeft;
+    this.initialY = getById("holder").offsetTop;
+    
+    var self = this;
+    window.onmousemove = function(e){
+        self.player.updatePosition(e.clientX - self.initialX, e.clientY - self.initialY);
+        console.log(self.initialX);
+    };
     
     this.loop();
 }
--- dev/extract2/js/Player.js
+++ dev/extract2/js/Player.js
@@ -1,8 +1,21 @@
 
-function PlayerController(){
-    
+function PlayerController(ctx){
+    this.ctx = ctx;
+    this.pos = {
+        x: 0,
+        y: 0
+    };
 }
 
 PlayerController.prototype.render = function(){
-    
+    this.ctx.clear();
+    this.ctx.fillStyle = "#9DF";
+    this.ctx.beginPath();
+    this.ctx.arc(this.pos.x, this.pos.y, 10, 0, PI2, 0);
+    this.ctx.fill();
+    this.ctx.closePath();
+};
+PlayerController.prototype.updatePosition = function(x, y){
+    this.pos.x = x;
+    this.pos.y = y;
 };
