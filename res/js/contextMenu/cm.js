8d23d8a76630f5c75dc4647ca7a454384f65a6aa
Guillaume Martigny <guillaume.martigny@gmail.com>
2014-10-22 17:06:53 +0200

    Better event handling Signed-off-by:Guillaume Martigny <guillaume.martigny@gmail.com>

--- res/js/contextMenu/cm.js
+++ res/js/contextMenu/cm.js
@@ -15,14 +15,16 @@ ContextMenu.createMenu = function(groups){
         s = document.createElement("div");
     m.id = "_cm";
     s.classList.add("holder");
-    
+
     groups.forEach(function(g){
         s.appendChild(ContextMenu.createMenu.group(g));
     });
-    
+
     m.addEventListener("contextmenu", function(e){
         e.preventDefault();
-        e.target.click();
+    });
+    m.addEventListener("click", function(e){
+        e.stopPropagation();
     });
     m.appendChild(s);
     return m;
@@ -32,23 +34,21 @@ ContextMenu.createMenu.group = function(g){
     d.classList.add("group");
     for(var o in g){
         if(g[o]){
-            if(g[o].length){
+            if(g[o].length)
                 d.appendChild(ContextMenu.createMenu.dropdown(o, g[o]));
-            }
-            else{
+            else
                 d.appendChild(ContextMenu.createMenu.option(o, g[o]));
-            }
         }
-        else{
+        else
             d.appendChild(ContextMenu.createMenu.noaction(o));
-        }
     }
     return d;
 };
-ContextMenu.createMenu.option = function(n,f){
+ContextMenu.createMenu.option = function(n, f){
     var i = document.createElement("button");
     i.innerHTML = n;
     i.addEventListener("click", f);
+    i.addEventListener("click", ContextMenu.callHide);
     return i;
 };
 ContextMenu.createMenu.noaction = function(n){
@@ -57,7 +57,7 @@ ContextMenu.createMenu.noaction = function(n){
     i.classList.add("no-action");
     return i;
 };
-ContextMenu.createMenu.dropdown = function(n,o){
+ContextMenu.createMenu.dropdown = function(n, o){
     var i = document.createElement("div"),
         s = document.createElement("div");
     i.innerHTML = n;
@@ -70,29 +70,33 @@ ContextMenu.createMenu.dropdown = function(n,o){
     return i;
 };
 ContextMenu.prototype.refreshHTML = function(){
-    if(this.menu) this.menu.remove();
+    if(this.menu)
+        this.menu.remove();
     this.menu = ContextMenu.createMenu(this.options);
 };
 ContextMenu.prototype.setOptions = function(options){
     this.options = options;
     this.refreshHTML();
 };
+ContextMenu.opened = false;
 ContextMenu.prototype.show = function(x, y){
     this.menu.style.left = x;
     this.menu.style.top = y;
     document.body.appendChild(this.menu);
-    
-    if(y+this.menu.offsetHeight > document.body.offsetHeight)
-        this.menu.style.top = y-this.menu.offsetHeight;
-    if(x+this.menu.offsetWidth > document.body.offsetWidth)
-        this.menu.style.left = document.body.offsetWidth-this.menu.offsetWidth;
-    
-    var cm = this;
-    window.addEventListener("click", function(){
-        cm.hide.call(cm);
-    });
+
+    if(y + this.menu.offsetHeight > document.body.offsetHeight)
+        this.menu.style.top = y - this.menu.offsetHeight;
+    if(x + this.menu.offsetWidth > document.body.offsetWidth)
+        this.menu.style.left = document.body.offsetWidth - this.menu.offsetWidth;
+
+    ContextMenu.opened = this;
+    window.addEventListener("click", ContextMenu.callHide);
+};
+ContextMenu.callHide = function(e){
+    console.log("click");
+    ContextMenu.opened.hide.call(ContextMenu.opened);
 };
 ContextMenu.prototype.hide = function(){
     this.menu.remove();
-    window.removeEventListener("click", this.hide);
+    window.removeEventListener("click", ContextMenu.callHide);
 };
