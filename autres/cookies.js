ee6db4224bdfa45757e6c3fa163a86c7c6b50db9
Guillaume Martigny <guillaume.martigny@gmail.com>
2015-02-23 11:21:17 +0100

    logic Signed-off-by:Guillaume Martigny <guillaume.martigny@gmail.com>

--- autres/cookies.js
+++ autres/cookies.js
@@ -56,13 +56,13 @@ Game.customLogic.push(function(){
         o = Game.Objects[o];
         if(!o.locked){
             val = C.simu.buyBuilding(o) / o.getPrice();
-            if(worst.val === null || worst.val > val){
-                worst.object = o;
-                worst.val = val;
-            }
             if(best.val === null || best.val < val){
                 best.object = o;
                 best.val = val;
+            }
+            else if(worst.val === null || worst.val > val){
+                worst.object = o;
+                worst.val = val;
             }
         }
     }
@@ -70,29 +70,14 @@ Game.customLogic.push(function(){
     worst.object = null; worst.val = null;
     if(best.object) best.object.l.classList.add("best");
     best.object = null; best.val = null;
-    
-    // All upgrades
-//    for(var u in Game.Upgrades){
-//        val = C.simu.buyUpgrade(u) / u.getPrice();
-//        if(worst.val === null || worst.val > val){
-//            worst.object = u;
-//            worst.val = val;
-//        }
-//        if(best.val === null || best.val < val){
-//            best.object = u;
-//            best.val = val;
-//        }
-//    }
-//    if(worst.object) worst.object.l.classList.add("worst"); worst.val = null;
-//    if(best.object) best.object.l.classList.add("best"); best.val = null;
 });
-Game.customInit = function(){
-    var s = document.createElement("style");
-    s.type = "text/css";
-    s.innerHTML =   ".product .title.worst{ color: #EDC0C0 } " +
-                    ".product .title.best{ color: #F5EA5A } " +
-                    "#upgrades .upgrade:after{ content: ''; position: absolute; width: 100%; height: 100%; border: 2px solid #BBB} " +
-                    ".upgrade.worst:after{ border-color: #EDC0C0; } " +
-                    ".upgrade.best:after{ border-color: #F5EA5A; }";
-    document.head.appendChild(s);
-};
+
+var s = document.createElement("style");
+s.type = "text/css";
+s.innerHTML =   ".product .title.worst{ color: #EDC0C0 } " +
+                ".product .title.best{ color: #F5EA5A } " +
+                "#upgrades .upgrade:after{ content: ''; position: absolute; width: 100%; height: 100%; border: 2px solid #BBB} " +
+                ".upgrade.worst:after{ border-color: #EDC0C0; } " +
+                ".upgrade.best:after{ border-color: #F5EA5A; }";
+document.head.appendChild(s);
+Game.Win('Third-party');
