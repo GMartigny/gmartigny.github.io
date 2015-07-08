3c4a7cb072249848ec91e24c0a52ae61f9c4af30
Guigui <Guigui@10.0.10.229>
2015-07-08 10:56:44 +0200

    random generated salamander

---
+++ dev/froggy/data.js
@@ -0,0 +1,80 @@
+var Colors = [
+    {
+        name: "Ebenus",     // Black
+        code: "#2E1F14"
+    },
+    {
+        name: "Albus",       // White
+        code: "#FBFEFE"
+    },
+    {
+        name: "Viridis",    // LightGreen
+        code: "#91E445"
+    },
+    {
+        name: "Rubeus",     // BrightRed
+        code: "#CD1F1F"
+    },
+    {
+        name: "Rutilus",    // Copper
+        code: "#F0B324"
+    },
+    {
+        name: "Flavus",     // Yellow
+        code: "#E9EC5E"
+    },
+    {
+        name: "Fuscus",     // Brown
+        code: "#6D532E"
+    },
+    {
+        name: "Violaceus",  // Violet
+        code: "#C872FC"
+    },
+    {
+        name: "Caeruleus",  // Blue
+        code: "#3153DD"
+    },
+    {
+        name: "Aureus",     // Gold
+        code: "#C9C15F"
+    },
+    {
+        name: "Argenteus",  // Silver
+        code: "#BFCED2"
+    },
+    {
+        name: "Purpureus",  // Purple
+        code: "#89099B"
+    }
+];
+var Shapes = [
+    {
+        name: "Stella",     // Star
+        src: "stella.png"
+    },
+    {
+        name: "Punctum",    // Dots
+        src: "punctum.png"
+    },
+    {
+        name: "Spiralis",   // Spiral
+        src: "spiralis.png"
+    },
+    {
+        name: "Nubes",      // Cloud
+        src: "nubes.png"
+    },
+    {
+        name: "Puslum",       // Puzzle
+        src: "puslum.png"
+    },
+    {
+        name: "Folia",       // Leaf
+        src: "folia.png"
+    },
+    {
+        name: "Riga",       // Leaf
+        src: "riga.png"
+    },
+];
