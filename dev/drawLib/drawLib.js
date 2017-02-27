dbf4d7f7e78f3fcb57be8484cfb6cb09c247246d
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2017-02-27 18:28:25 +0100

    Improve drawLib and add flubber example

--- dev/drawLib/drawLib.js
+++ dev/drawLib/drawLib.js
@@ -2,11 +2,22 @@ var Utils = {
     PI: Math.PI,
     sin: Math.sin,
     cos: Math.cos,
+    sq: function(x) {
+        return Math.pow(x, 2);
+    },
+    sqrt: Math.sqrt,
+    rand: Math.random,
     floor: function(x) {
         return x | 0;
     },
     round: function(x) {
-        return Utils.floor(x + 0.5);
+        return this.floor(x + .5);
+    },
+    ceil: function(x) {
+        return this.floor(x + 1);
+    },
+    random: function(max) {
+        return this.round(this.rand() * max);
     },
     /**
      * Create a class prototype from a parent
@@ -120,7 +131,10 @@ Scene.prototype = {
      * @return {Position}
      */
     center: function() {
-        return new Position(this.width() / 2 << 0, this.height() / 2 << 0);
+        return new Position(Utils.floor(this.width() / 2), Utils.floor(this.height() / 2));
+    },
+    randomPosition: function() {
+        return new Position(Utils.random(this.width()), Utils.random(this.height()));
     },
     /**
      * Get the width of the scene
@@ -215,7 +229,7 @@ Position.prototype = {
     /**
      * Add to the x value
      * @param {Number} diff - How much to add
-     * @param {Boolean} override - If true, will change the origin value
+     * @param {Boolean} [override=false] - If true, will change the origin value
      * @return {Position} Itself
      */
     addX: function(diff, override) {
@@ -232,7 +246,7 @@ Position.prototype = {
     /**
      * Add to the y value
      * @param {Number} diff - How much to add
-     * @param {Boolean} override - If true, will change the origin value
+     * @param {Boolean} [override=false] - If true, will change the origin value
      * @return {Position} Itself
      */
     addY: function(diff, override) {
@@ -314,7 +328,7 @@ function Animation (func) {
  * @param {Boolean} [counterClockWise=false] - Circle counter clockwise
  * @return {Animation}
  */
-Animation.Cirlce = function(radius, speed, counterClockWise) {
+Animation.Circle = function(radius, speed, counterClockWise) {
     Utils.assertLength(arguments, 1);
     speed = speed || .1;
     return new Animation(function(i) {
@@ -350,7 +364,7 @@ Animation.Gravity = function(ground, bounce) {
         ground = ground || ctx.canvas.height;
         if (this.getY() >= ground && i > 0) {
             this.setY(ground);
-            this.animation.iteration = (-i * bounce) << 0;
+            this.animation.iteration = Utils.floor(-i * bounce);
         }
         else {
             this.addY(0.09 * i);
@@ -476,16 +490,30 @@ function Polygon (points, options) {
 
     this.position = new Position();
     this.options = options || {};
-    this.points = points;
+    this.points = [];
+    this.isLinkedToOthers = false;
 
     var sumX = 0;
     var sumY = 0;
-    var l = this.points.length;
+    var l = points.length;
     for (var i = 0; i < l; ++i) {
-        var p = this.points[i];
-        sumX += p.getX();
-        sumY += p.getY();
-        this.position.addLink(p);
+        var p;
+        if (points[i] instanceof Shape) {
+            this.isLinkedToOthers = true;
+            p = Position.createFrom(points[i]);
+        }
+        else if (points[i] instanceof Position) {
+            p = points[i];
+        }
+        if (p) {
+            sumX += p.getX();
+            sumY += p.getY();
+            this.position.addLink(p);
+            this.points.push(p);
+        }
+        else {
+            throw new TypeError("Polygon can only link positions or shapes, but " + points[i].constructor.name + " given.");
+        }
     }
     this.position.setOrigin(sumX / l, sumY / l);
 }
@@ -496,11 +524,94 @@ Utils.extends(Polygon, Shape, /** @lends Polygon.prototype */ {
      * @param {CanvasRenderingContext2D} ctx - A drawing context
      */
     trace: function(ctx) {
-        ctx.moveTo(this.points[0].getX(), this.points[0].getY());
+        var first = this.points[0];
+        ctx.moveTo(first.getX(), first.getY());
 
         for (var i = 1, l = this.points.length; i < l; ++i) {
             var p = this.points[i];
             ctx.lineTo(p.getX(), p.getY());
+        }
+
+        ctx.lineTo(first.getX(), first.getY());
+    },
+    /**
+     * Check if polygon can be animated
+     * @override Shape.animateWith
+     * @param {Animation} animation - Any animation
+     */
+    animateWith: function(animation) {
+        if (this.isLinkedToOthers) {
+            throw new ReferenceError("Can't animate polygon with link to other.");
+        }
+        else {
+            this._animateWith(animation);
+        }
+    }
+});
+
+/**
+ * A shape from multiple point with rounded angle
+ * @param {Array<Position|Shape>} points - A list of points
+ * @param {Number} tension - Define the roundness of the blob
+ * @param {Object} options - Specific options for this shape
+ * @constructor
+ */
+function Blob (points, tension, options) {
+    this.tension = tension || 0.5;
+
+    Polygon.call(this, points, options);
+}
+/**
+ * Return an array of coordinates for control points around a point
+ * @param {Position} p0 - Previous point
+ * @param {Position} p1 - Current point
+ * @param {Position} p2 - Next Point
+ * @param {Number} tension - Roundness of angles
+ * @return {Array<Number>}
+ * @private
+ * @static
+ */
+Blob._getControlPoints = function (p0, p1, p2, tension) {
+    var x0 = p0.getX();
+    var x1 = p1.getX();
+    var x2 = p2.getX();
+    var y0 = p0.getY();
+    var y1 = p1.getY();
+    var y2 = p2.getY();
+
+    var d01 = Utils.sqrt(Utils.sq(x1 - x0) + Utils.sq(y1 - y0));
+    var d12 = Utils.sqrt(Utils.sq(x2 - x1) + Utils.sq(y2 - y1));
+
+    var fa = tension * d01 / (d01 + d12);
+    var fb = tension - fa;
+
+    var p1x = x1 + fa * (x0 - x2);
+    var p1y = y1 + fa * (y0 - y2);
+
+    var p2x = x1 - fb * (x0 - x2);
+    var p2y = y1 - fb * (y0 - y2);
+
+    return [p1x, p1y, p2x, p2y];
+};
+Utils.extends(Blob, Polygon, /** @lends Blob.prototype */ {
+    /**
+     * Trace the blob
+     * @override Polygon.trace
+     * @param {CanvasRenderingContext2D} ctx - A drawing context
+     */
+    trace: function(ctx) {
+        var cp = [];
+        var n = this.points.length;
+        for (var i = 0; i < n; ++i) {
+            cp[(i + 1) % n] = Blob._getControlPoints(this.points[i], this.points[(i + 1) % n], this.points[(i + 2) % n], this.tension);
+        }
+        ctx.moveTo(this.points[0].getX(), this.points[0].getY());
+        for (i = 0; i < n; ++i) {
+            ctx.bezierCurveTo(
+                cp[i][2], cp[i][3],
+                cp[(i + 1) % n][0], cp[(i + 1) % n][1],
+                this.points[(i + 1) % n].getX(), this.points[(i + 1) % n].getY()
+            );
         }
     }
 });
@@ -599,6 +710,7 @@ function Rectangle (startPoint, width, height, options) {
 Rectangle.fromPointToPoint = function(from, to, options) {
     var rect = new Rectangle(from, 0, 0, options);
     rect.endPoint = Position.createFrom(to);
+    rect.isLinkedToOthers = true;
     return rect;
 };
 Utils.extends(Rectangle, Shape, /** @lends Rectangle.prototype */{
@@ -612,14 +724,6 @@ Utils.extends(Rectangle, Shape, /** @lends Rectangle.prototype */{
             this.endPoint.getX() - this.startPoint.getX(),
             this.endPoint.getY() - this.startPoint.getY()
         );
-    },
-    animateWith: function(animation) {
-        if (!this.endPoint.isLinked) {
-            this._animateWith(animation);
-        }
-        else {
-            throw new ReferenceError("Can't animate rectangle created from two point");
-        }
     },
     /**
      *
