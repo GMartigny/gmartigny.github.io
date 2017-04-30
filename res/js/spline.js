473d0c2113c23c3f548cd88b3089d2864ab109e7
Guillaume Martigny <guillaume.martigny@gmail.com>
2017-04-30 20:23:51 +0200

    wrap

--- res/js/spline.js
+++ res/js/spline.js
@@ -2,6 +2,10 @@
  * Code copied and adapted from http://scaledinnovation.com/analytics/splines/aboutSplines.html
  * By Rob Spencer (thanks dude)
  */
+(function () {
+    var sqrt = Math.sqrt,
+        pow = Math.pow,
+        sq = function(x){ return pow(x, 2); };
 
 /**
  * Draw a curve through points
